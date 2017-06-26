5598102c2b8e68f46e8dc88ac7b1c3afbf262d11
zoe-c <find.zoec@gmail.com>
2017-06-26 16:51:28 -0400

    get index render working

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const mustache = require('mustache-express');
 // const validator = require('express-validator');
-const User = require('./models/user.js');
+const users = require('./models/user.js');
 
 
 // CREATE APP
@@ -25,67 +25,50 @@ app.set('views', './views');
 app.set('view engine', 'mustache');
 
 // MIDDLEWARE-------------------------
-//Parser mid.w
+//Parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Session mid.w
+// Session middleware
 app.use(session({
   secret: 'keyboard dog',
   resave: false,
   saveUninitialized: true
 }))
 
+// // Amy's custom- MW FUNCTION FOR AUTH
+function authenticate(req, username, password) {
+  var authenticatedUser = users.find(function (user) {
+    if (username === user.username && password === user.password) {
+      req.session.authenticated = true;
+      console.log('User & Password Authenticated');
+    } else {
+      return false;
+    }
+  });
+  console.log(req.session);
+  return req.session;
+}
 
-// STATIC FILE/ DIR -----------------------------------
-// Styles
-app.use(express.static('public'));
+// // STATIC FILE/ DIR -----------------------------------
+// // Styles *******NOTE:WORRY ABOUT STYLES LATER
+// app.use(express.static('public'));
 
-
- // app.use(function (req, res, next) {
-//   var views = req.session.views
-//
-//   if (!views) {
-//     views = req.session.views = {}
-//   }
-//
-//   // get the url pathname
-//   var pathname = parseurl(req).pathname
-//
-//   // count the views
-//   views[pathname] = (views[pathname] || 0) + 1
-//
-//   next()
-// })
-//
-//
-// app.get('/foo', function (req, res, next) {
-//   res.send('you viewed this page ' + req.session.views['/foo'] + ' times')
-// })
-//
-// app.get('/bar', function (req, res, next) {
-//   res.send('you viewed this page ' + req.session.views['/bar'] + ' times')
-// })
 
 // APP.GET()
-// app.get('/', function (req, res) {
-//    res.redirect('/login/');
-// })
-app.get('/login/', function (req, res) {
-   res.render('login', function authenticate(req, username, password) {
-   var authenticatedUser = data.users.find(funciton(user) {
-      if (user.username === username && user.password === password) {
-         req.session.authentication = true;
-         console.log('User and Password Authenticated')
-      } else {
-         return false;
-      }
-      return req.session;
-
-   });
+app.get('/', function (req, res) {
+   // authenticate(req, username, password);
+   if (req.session && req.session.authenticated) {
+      res.render('index', {username : username});
+   } else {
+      res.redirect('/login/');
+   }
 });
 
+app.get('/login/',function(req, res){
+   res.render('login', console.log('HERE IS YOUR FORM'));
 });
+
 
 // APP.POST
 app.post('/login/', function(req, res){
@@ -93,8 +76,9 @@ app.post('/login/', function(req, res){
    var password = req.body.password;
    authenticate(req, username, password);
    if (req.session && req.session.authenticated) {
-      res.redirect('/');
-      res.render('index', {username : username})
+      console.log("you are authenticated!");
+      // res.redirect('/');
+      // res.render('index', {username : username})
    }
 })
 
